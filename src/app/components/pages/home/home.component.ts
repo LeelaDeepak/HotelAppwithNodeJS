730a8e3e55ac04be0116f7d9e6bbcd3dd18034ca
Sublari Leela Deepak <leeladeepak123@gmail.com>
2024-07-02 13:41:59 +0530

    Search changes

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -2,21 +2,30 @@ import { Component } from '@angular/core';
 import { Food } from '../../../shared/models/food';
 import { FoodService } from '../../../services/food.service';
 import { CurrencyPipe } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { SearchComponent } from '../../partials/search/search.component';
+
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink,CurrencyPipe],
+  imports: [RouterLink,CurrencyPipe,SearchComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
   foods:Food[]=[];
-  constructor(private foodService:FoodService) {
-    this.foods=this.foodService.getAll();
+  constructor(private foodService:FoodService,private activatedRoute:ActivatedRoute) {
+    activatedRoute.params.subscribe((params)=>{
+      if(params['searchTerm']){
+        this.foods = this.foodService.getAllFoodsBySearchTerm(params['searchTerm']);
+      }else{
+        this.foods=this.foodService.getAll();
+      }
+    })
+    
   }
 
   
